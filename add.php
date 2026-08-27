fa0363e980b63821cfcf061766bb46e56d6404cf
arifnazhannorli-spec <arifnazhan.norli@student.gmi.edu.my>
2026-08-27 20:09:58 +0800

    Video 19 - basic form validation

--- add.php
+++ add.php
@@ -1,47 +1,28 @@
 <?php
 
-// check if form kena submit
 if(isset($_POST['submit'])){
 
-    // make input safer before show it
-    echo htmlspecialchars($_POST['email']);
-    echo htmlspecialchars($_POST['title']);
-    echo htmlspecialchars($_POST['ingredients']);
+    // check email
+    if(empty($_POST['email'])){
+        echo 'An email is required <br />';
+    } else {
+        echo htmlspecialchars($_POST['email']);
+    }
 
-}
+    // check title
+    if(empty($_POST['title'])){
+        echo 'A title is required <br />';
+    } else {
+        echo htmlspecialchars($_POST['title']);
+    }
+
+    // check ingredients
+    if(empty($_POST['ingredients'])){
+        echo 'At least one ingredient is required <br />';
+    } else {
+        echo htmlspecialchars($_POST['ingredients']);
+    }
+
+} // end of POST check
 
 ?>
-
-<!DOCTYPE html>
-<html>
-
-    <?php include('templates/header.php'); ?>
-
-    <section class="container grey-text">
-
-        <h4 class="center">Add a Pizza</h4>
-
-        <form class="white" action="add.php" method="POST">
-
-            <label>Your Email:</label>
-            <input type="text" name="email">
-
-            <label>Pizza Title:</label>
-            <input type="text" name="title">
-
-            <label>Ingredients (comma separated):</label>
-            <input type="text" name="ingredients">
-
-            <div class="center">
-
-                <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
-
-            </div>
-
-        </form>
-
-    </section>
-
-    <?php include('templates/footer.php'); ?>
-
-</html>
