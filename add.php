dc1e7f41921962168ec51527aa8c23182986dad6
arifnazhannorli-spec <arifnazhan.norli@student.gmi.edu.my>
2026-08-27 20:23:22 +0800

    Video 21 - display form errors

--- add.php
+++ add.php
@@ -1,40 +1,46 @@
 <?php
 
+// start empty first
+$title = $email = $ingredients = '';
+
+// keep all errors here
+$errors = array('email' => '', 'title' => '', 'ingredients' => '');
+
 if(isset($_POST['submit'])){
 
     // check email
     if(empty($_POST['email'])){
-        echo 'An email is required <br />';
+        $errors['email'] = 'An email is required <br />';
     } else {
         $email = $_POST['email'];
 
-        // check email betul ke tak
+        // check valid email
         if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-            echo 'email must be a valid email address';
+            $errors['email'] = 'email must be a valid email address';
         }
     }
 
     // check title
     if(empty($_POST['title'])){
-        echo 'A title is required <br />';
+        $errors['title'] = 'A title is required <br />';
     } else {
         $title = $_POST['title'];
 
-        // title only letters and spaces
+        // letters and spaces only
         if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
-            echo 'Title must be letters and spaces only';
+            $errors['title'] = 'Title must be letters and spaces only';
         }
     }
 
     // check ingredients
     if(empty($_POST['ingredients'])){
-        echo 'At least one ingredient is required <br />';
+        $errors['ingredients'] = 'At least one ingredient is required <br />';
     } else {
         $ingredients = $_POST['ingredients'];
 
-        // make sure ingredients separated by comma
+        // check comma separated list
         if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
-            echo 'Ingredients must be a comma separated list';
+            $errors['ingredients'] = 'Ingredients must be a comma separated list';
         }
     }
 
@@ -45,33 +51,34 @@ if(isset($_POST['submit'])){
 <!DOCTYPE html>
 <html>
 
-<?php include('templates/header.php'); ?>
+    <?php include('templates/header.php'); ?>
 
-<section class="container grey-text">
+    <section class="container grey-text">
 
-    <h4 class="center">Add a Pizza</h4>
+        <h4 class="center">Add a Pizza</h4>
 
-    <form class="white" action="add.php" method="POST">
+        <form class="white" action="add.php" method="POST">
 
-        <label>Your Email:</label>
-        <input type="text" name="email">
+            <label>Your Email:</label>
+            <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+            <div class="red-text"><?php echo $errors['email']; ?></div>
 
-        <label>Pizza Title:</label>
-        <input type="text" name="title">
+            <label>Pizza Title:</label>
+            <input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
+            <div class="red-text"><?php echo $errors['title']; ?></div>
 
-        <label>Ingredients (comma separated):</label>
-        <input type="text" name="ingredients">
+            <label>Ingredients (comma separated):</label>
+            <input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
+            <div class="red-text"><?php echo $errors['ingredients']; ?></div>
 
-        <div class="center">
+            <div class="center">
+                <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+            </div>
 
-            <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+        </form>
 
-        </div>
+    </section>
 
-    </form>
-
-</section>
-
-<?php include('templates/footer.php'); ?>
+    <?php include('templates/footer.php'); ?>
 
 </html>
