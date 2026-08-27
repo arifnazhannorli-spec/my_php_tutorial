ff231499435b7083411bf03930e620766e97cf92
arifnazhannorli-spec <arifnazhan.norli@student.gmi.edu.my>
2026-08-27 20:29:00 +0800

    Video 22 - finish form validation

--- add.php
+++ add.php
@@ -3,45 +3,51 @@
 // start empty first
 $title = $email = $ingredients = '';
 
-// keep all errors here
+// keep the errors here
 $errors = array('email' => '', 'title' => '', 'ingredients' => '');
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
 
     // check email
-    if(empty($_POST['email'])){
+    if (empty($_POST['email'])) {
         $errors['email'] = 'An email is required <br />';
     } else {
         $email = $_POST['email'];
 
-        // check valid email
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $errors['email'] = 'email must be a valid email address';
         }
     }
 
     // check title
-    if(empty($_POST['title'])){
+    if (empty($_POST['title'])) {
         $errors['title'] = 'A title is required <br />';
     } else {
         $title = $_POST['title'];
 
-        // letters and spaces only
-        if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+        if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
             $errors['title'] = 'Title must be letters and spaces only';
         }
     }
 
     // check ingredients
-    if(empty($_POST['ingredients'])){
+    if (empty($_POST['ingredients'])) {
         $errors['ingredients'] = 'At least one ingredient is required <br />';
     } else {
         $ingredients = $_POST['ingredients'];
 
-        // check comma separated list
-        if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+        if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
             $errors['ingredients'] = 'Ingredients must be a comma separated list';
         }
+    }
+
+
+    // check if got any errors
+    if (array_filter($errors)) {
+        // errors in the form
+    } else {
+        // all good, go back home
+        header('Location: index.php');
     }
 
 } // end of POST check
@@ -51,34 +57,34 @@ if(isset($_POST['submit'])){
 <!DOCTYPE html>
 <html>
 
-    <?php include('templates/header.php'); ?>
+<?php include('templates/header.php'); ?>
 
-    <section class="container grey-text">
+<section class="container grey-text">
 
-        <h4 class="center">Add a Pizza</h4>
+    <h4 class="center">Add a Pizza</h4>
 
-        <form class="white" action="add.php" method="POST">
+    <form class="white" action="add.php" method="POST">
 
-            <label>Your Email:</label>
-            <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
-            <div class="red-text"><?php echo $errors['email']; ?></div>
+        <label>Your Email:</label>
+        <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+        <div class="red-text"><?php echo $errors['email']; ?></div>
 
-            <label>Pizza Title:</label>
-            <input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
-            <div class="red-text"><?php echo $errors['title']; ?></div>
+        <label>Pizza Title:</label>
+        <input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
+        <div class="red-text"><?php echo $errors['title']; ?></div>
 
-            <label>Ingredients (comma separated):</label>
-            <input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
-            <div class="red-text"><?php echo $errors['ingredients']; ?></div>
+        <label>Ingredients (comma separated):</label>
+        <input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
+        <div class="red-text"><?php echo $errors['ingredients']; ?></div>
 
-            <div class="center">
-                <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
-            </div>
+        <div class="center">
+            <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+        </div>
 
-        </form>
+    </form>
 
-    </section>
+</section>
 
-    <?php include('templates/footer.php'); ?>
+<?php include('templates/footer.php'); ?>
 
 </html>
