1cedcadde79865ff9039f4b7ac5599a79d67cd28
arifnazhannorli-spec <arifnazhan.norli@student.gmi.edu.my>
2026-08-27 20:15:58 +0800

    Video 20 - form validation

--- add.php
+++ add.php
@@ -6,23 +6,72 @@ if(isset($_POST['submit'])){
     if(empty($_POST['email'])){
         echo 'An email is required <br />';
     } else {
-        echo htmlspecialchars($_POST['email']);
+        $email = $_POST['email'];
+
+        // check email betul ke tak
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            echo 'email must be a valid email address';
+        }
     }
 
     // check title
     if(empty($_POST['title'])){
         echo 'A title is required <br />';
     } else {
-        echo htmlspecialchars($_POST['title']);
+        $title = $_POST['title'];
+
+        // title only letters and spaces
+        if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+            echo 'Title must be letters and spaces only';
+        }
     }
 
     // check ingredients
     if(empty($_POST['ingredients'])){
         echo 'At least one ingredient is required <br />';
     } else {
-        echo htmlspecialchars($_POST['ingredients']);
+        $ingredients = $_POST['ingredients'];
+
+        // make sure ingredients separated by comma
+        if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+            echo 'Ingredients must be a comma separated list';
+        }
     }
 
 } // end of POST check
 
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<?php include('templates/header.php'); ?>
+
+<section class="container grey-text">
+
+    <h4 class="center">Add a Pizza</h4>
+
+    <form class="white" action="add.php" method="POST">
+
+        <label>Your Email:</label>
+        <input type="text" name="email">
+
+        <label>Pizza Title:</label>
+        <input type="text" name="title">
+
+        <label>Ingredients (comma separated):</label>
+        <input type="text" name="ingredients">
+
+        <div class="center">
+
+            <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+
+        </div>
+
+    </form>
+
+</section>
+
+<?php include('templates/footer.php'); ?>
+
+</html>
